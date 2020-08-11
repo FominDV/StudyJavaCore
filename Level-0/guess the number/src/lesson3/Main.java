6aabbe408bfb111275652491d6177dd26a52afbe
Dmitriy Fomin <79067773397@yandex.ru>
2020-08-12 00:32:16 +0300

    Done lesson-3

--- Level-0/guess the number/src/lesson3/Main.java
+++ Level-0/guess the number/src/lesson3/Main.java
@@ -6,8 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Your task is to guess the number");
-        int range = 10;
-        
+        System.out.println("Your task is to guess the number\nEnter count of numbers");
+        int range = scn.nextInt();
+        int number = (int)(Math.random()*range);
+        while(true){
+            System.out.println("Enter your number from 0 to " + (range-1));
+            int inputNumber = scn.nextInt();
+            if(inputNumber == number) {
+                System.out.println("You WIN! It is really " + number);
+                break;
+            } else if(inputNumber > number) {
+                System.out.println("Hidden number is less than " + inputNumber);
+            } else {
+                System.out.println("Hidden number is greater than " + inputNumber);
+            }
+
+        }
+        scn.close();
     }
 }
