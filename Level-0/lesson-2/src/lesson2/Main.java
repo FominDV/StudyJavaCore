186f18e48128d8225ba649f7c4462cd1dcd23fac
Dmitriy Fomin <79067773397@yandex.ru>
2020-08-11 22:35:18 +0300

    create lesson-2

---
+++ Level-0/lesson-2/src/lesson2/Main.java
@@ -0,0 +1,23 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Enter the operation");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraction");
+        System.out.println("3 - Multiplication");
+        Scanner scan = new Scanner(System.in);
+        int operation = scan.nextInt();
+        System.out.println("Enter the first number");
+        int FirstNan = scan.nextInt();
+        System.out.println("Enter second number");
+        int SecondNan = scan.nextInt();
+        int result;
+        
+
+    }
+}
