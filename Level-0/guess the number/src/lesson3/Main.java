43d49f2687e173bfbe76bce0d5146176ce8acd87
Dmitriy Fomin <79067773397@yandex.ru>
2020-08-11 23:54:01 +0300

    Start "guess the number"

---
+++ Level-0/guess the number/src/lesson3/Main.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Your task is to guess the number");
+        int range = 10;
+        
+    }
+}
