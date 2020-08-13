c2c5338b2fa99bf4088e11f1defff9602b2d92b5
Dmitriy Fomin <79067773397@yandex.ru>
2020-08-13 23:00:27 +0300

    start lesson 7

--- Level-0/catch the drops/src/catchDrops/GameWindow.java
+++ Level-0/catch the drops/src/catchDrops/GameWindow.java
@@ -1,18 +1,30 @@
 package catchDrops;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
-
+    private static Image game_over;
+    private static Image background;
+    private static Image drop;
+    private static long time;
     private static GameWindow GW;
+    private static float dropX = 200;
+    private static float dropY = -100;
+    private static float dropV = 200;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        background = ImageIO.read(GameWindow.class.getResourceAsStream("background.png"));
+        game_over = ImageIO.read(GameWindow.class.getResourceAsStream("game_over.png"));
+        drop = ImageIO.read(GameWindow.class.getResourceAsStream("drop.png"));
         GW = new GameWindow();
         GW.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//close window
         GW.setLocation(200,100);//window appearance point
         GW.setSize(906, 478); //size of window
         GW.setResizable(false); //Forbidding window changes
+        time = System.nanoTime();
         gameField GF = new gameField();
         GW.add(GF);
         GW.setVisible(true);//make it visible
@@ -22,8 +34,13 @@ public class GameWindow extends JFrame {
     }
 
     private static void Repaint(Graphics g){
-        g.fillOval(10,10,200,100);
-        g.drawLine(10,110,210,210);
+        long current_time = System.nanoTime();
+        float delta_time = (current_time - time) * 0.000000001f;
+        time = current_time;
+        dropY += dropV * delta_time;
+        g.drawImage(background, 0,0,null);
+        g.drawImage(drop,(int) dropX,(int) dropY,null);
+        //g.drawImage(game_over, 280,120,null);
 
     }
 
@@ -32,6 +49,7 @@ public class GameWindow extends JFrame {
         protected void paintComponent(Graphics g){
             super.paintComponent(g);  //draws Jpanel
             Repaint(g);
+            repaint();
 
         }
 
