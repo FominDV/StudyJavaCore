2cae8fd226c53b3d724b05e8164eca003ab64eee
Dmitriy Fomin <79067773397@yandex.ru>
2020-12-13 12:03:07 +0300

    task 8-9

--- philosophy_of_java/src/Test3.java
+++ philosophy_of_java/src/Test3.java
@@ -3,5 +3,10 @@ public class Test3 {
         //a=7
 long a=07777777777770L;
 System.out.println(Long.toOctalString(a));
+float f=3.4e-2f;
+System.out.println("f= "+(f));
+int hex1=0xf1;
+int hex2=0xf0;
+System.out.println(Integer.toBinaryString(hex1^hex2));
     }
 }
