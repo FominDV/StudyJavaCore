42d804f7052db3c33dbc1950eda43e75b939f749
Dmitriy Fomin <79067773397@yandex.ru>
2021-01-23 06:55:46 +0300

    REST_example

---
+++ rest_exampl/src/main/java/ru/fomin/rest_exampl/RestExamplApplication.java
@@ -0,0 +1,13 @@
+package ru.fomin.rest_exampl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExamplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestExamplApplication.class, args);
+    }
+
+}
---
+++ rest_exampl/src/test/java/ru/fomin/rest_exampl/RestExamplApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fomin.rest_exampl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExamplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
